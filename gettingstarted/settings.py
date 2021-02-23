dac93afe64668a29640573905df0f2c89ca9d365
Somesh vyas <somesh.vyas@faasos.com>
2021-02-23 16:40:49 +0530

    logout link

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -116,5 +116,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/' # new
 
 django_heroku.settings(locals())
