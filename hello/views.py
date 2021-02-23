ded9a1046db67f23ce3da00c17ace967f774aa31
Somesh vyas <somesh.vyas@faasos.com>
2021-02-23 16:54:36 +0530

    requests

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import requests
 from .models import Greeting
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    response = requests.get('https://api.imgflip.com/get_memes')
+    memes = response.json()
+    return render(request, "index.html", {'memes': memes})
 
 
 def db(request):
