62e5f6be580275c9e91189da254bef6ffedea108
Somesh vyas <somesh.vyas@faasos.com>
2021-02-24 09:39:47 +0530

    migration

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+
+class Cookie(models.Model):
+    user = models.CharField(max_length=200)
+    consent = models.BooleanField(default=false)
